5213a569e6d15d20de4cfb4cc9583885ca720b8c
gabrielrna7 <158219579+gabrielrna7@users.noreply.github.com>
2024-01-30 17:34:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function gerarRelatorio() {
+    var textoPadrao = document.getElementById("textoPadrao").value;
+    var relatorio = "";
+
+    // Lógica para gerar o relatório (pode ser similar ao exemplo anterior)
+
+    document.getElementById("relatorio").innerText = relatorio;
+}
